b2c56a6634529d48caddc4cb71fff915bd329e04
Kat Vengo <katvengo@gmail.com>
2019-03-16 12:28:07 -0700

    almost finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,76 +1,101 @@
 $(document).ready(function () {
 
-    var win
-    var loss
-    var numbers = [];
+    var winner = document.getElementsByClassName('winner');
+    var loser = document.getElementsByClassName('loser');
+    var compNum = document.getElementsByClassName('bigger1');
+    var totalScore = document.getElementsByClassName('totalscore');
+    var total = 0;
 
-    var gem1 = 0
-    var gem2 = 0
-    var gem3 = 0
-    var gem4 = 0
-
-    var score
-    var totalScore = document.getElementById('totalscore');
-
-    setRandomNumber()
-
-
-    function setRandomNumber() { //Whenever we refresh the page it changes the random number
-        $('.bigger1').text(Math.floor((Math.random() * 50) + 1))
+    function setRandomNumber() { 
+        return (Math.floor((Math.random() * 50) + 1));
     }
 
-    function setRandomSmallNumber() { //everytime we click on the crystal the number changes, need it to stay the same
+    function setRandomSmallNumber() { 
         return (Math.floor((Math.random() * 10) + 1))
     }
 
+    var pinkcrystal = setRandomSmallNumber();
+    var diamond = setRandomSmallNumber();
+    var purplecrystal = setRandomSmallNumber();
+    var redcrystal = setRandomSmallNumber();
+
     function reset() {
-        score = 0
-        var randNum = setRandomSmallNumber();
-        var pinkcrystal = setRandomSmallNumber();
-        var diamond = setRandomSmallNumber();
-        var purplecrystal = setRandomSmallNumber();
-        var redcrystal = setRandomSmallNumber();
+        compNum = setRandomNumber()
+        $(".bigger1").text(compNum)
+        total = 0
+        pinkcrystal = setRandomSmallNumber();
+        diamond = setRandomSmallNumber();
+        purplecrystal = setRandomSmallNumber();
+        redcrystal = setRandomSmallNumber();
         $('.pinkCrystal').val(pinkcrystal);
         $('.diamond').val(diamond);
-        $('.purplecrystal').val(purplecrystal);
+        $('.purpleCrystal').val(purplecrystal);
         $('.redCrystal').val(redcrystal);
-        
     }
 
 
-
+    reset()
 
     $('.pinkCrystal').on('click', function () { //Here we call the function so that when we click we get a random number
-        reset();
         console.log('I am the pink crystal!')
         var pinkGemValue = parseInt($(this).val());
-        score += pinkGemValue
-        $('.totalscore').text(pinkGemValue);
+        console.log("pinkgemvalue", pinkGemValue)
+        total = total + pinkGemValue;
+        console.log(total);
+        $(".totalscore").text(total)
+    
+
     })
 
     $('.diamond').on('click', function () {
-
         console.log('I am a diamond!')
+        var diamondGemValue = parseInt($(this).val());
+        console.log("diamondValue", diamondGemValue)
+        total = total + diamondGemValue;
+        console.log(total);
+        $(".totalscore").text(total)
     })
 
-    $('.purpleCrystal').on('click', function () {
-
+    $('.purpleCrystal').on('click', function () { //Showing NaN
         console.log('I am the purple crystal!')
+        var purpleGemValue = parseInt($(this).val());
+        console.log("purpleGemvalue", purpleGemValue)
+        total = total + purpleGemValue;
+        console.log(total);
+        $(".totalscore").text(total)
     })
 
-    $('.redCrystal').on('click', function () {
+    $('.redCrystal').on('click', function () { //Showing NaN
         console.log('I am the red crystal!')
+        var redGemValue = parseInt($(this).val());
+        console.log("redGemvalue", redGemValue)
+        total = total + redGemValue;
+        console.log(total);
+        $(".totalscore").text(total)
     })
 });
 
+if (total == compNum) {
+    winner++;
+    alert('You won!')
+    console.log('you won!')
+    reset()
+    $(".winner").text(winner)
+}
+if (total > compNum) {
+    loser++;
+    alert('You lost!')
+    reset()
+}
 
-// function userTotalScore(params) {
+if (total != compNum) {
+    loser++;
+    reset()
+}
 
-// }
 
 //First, I need to make sure that when we setRandomNumber it only resets when we win or lose the game 
 //I need to make sure that whenever we start a new game, the random number when we click on the crystal, stays the same
 //We need to be able to push the numbers of the crystals into an empty array
 //Then we need to combine the numbers and match them to the number in bigger1
 //If the numbers match, user wins and we increase our win total! If they do not, then we increase our losses by 1.
-
